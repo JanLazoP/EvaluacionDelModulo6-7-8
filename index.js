ad234c57102fb869a402ba2cc978c2489651c2e8
JLP993 <lazopizarro2121@gmail.com>
2026-09-13 03:59:29 -0300

    Leccion 5: implementa registro de visitas en /status

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import { error } from 'console';
 
 dotenv.config();
 
@@ -9,11 +11,27 @@ const PORT = process.env.PORT;
 app.use(express.static('public'));
 
 app.get('/status', (req,res) => {
+    registrarVisita('/status');
+
     res.json({
         status: 'ok',
         mensaje: 'Servidor funcionando correctamente'
     });
 });
+
+function registrarVisita(ruta){
+    const fecha = new Date();
+    const fechaFormateada = fecha.toLocaleDateString('es-CL');
+    const horaFormateada = fecha.toLocaleTimeString('es-CL');
+
+    const registro = `${fechaFormateada} - ${horaFormateada} - Ruta: ${ruta} \n`;
+
+    fs.appendFile('log.txt', registro, (error) => {
+        if(error){
+            console.error('Error al registrar la visita:', error);
+        }
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
