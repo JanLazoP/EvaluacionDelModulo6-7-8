8e87a7274efba6e22d22b04a7e4bf186a1e75e5a
JLP993 <lazopizarro2121@gmail.com>
2026-09-13 03:28:41 -0300

    agregar rutas públicas

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.static('public'));
+
+app.get('/status', (req,res) => {
+    res.json({
+        status: 'ok',
+        mensaje: 'Servidor funcionando correctamente'
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
 })
+
 
 
